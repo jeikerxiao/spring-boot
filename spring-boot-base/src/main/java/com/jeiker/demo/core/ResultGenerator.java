17bd4a9e6a4ab515f7834269a03ca5b6fed2972d
jeikerxiao <jeiker@126.com>
2017-07-31 14:50:40 +0800

    init spring-boot-base project
    init spring-boot-base project

---
+++ spring-boot-base/src/main/java/com/jeiker/demo/core/ResultGenerator.java
@@ -0,0 +1,29 @@
+package com.jeiker.demo.core;
+
+/**
+ * 响应结果生成工具
+ * @Author : xiao
+ * @Date : 17/7/31 下午2:35
+ */
+public class ResultGenerator {
+    private static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
+
+    public static Result genSuccessResult() {
+        return new Result()
+                .setCode(ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE);
+    }
+
+    public static Result genSuccessResult(Object data) {
+        return new Result()
+                .setCode(ResultCode.SUCCESS)
+                .setMessage(DEFAULT_SUCCESS_MESSAGE)
+                .setData(data);
+    }
+
+    public static Result genFailResult(String message) {
+        return new Result()
+                .setCode(ResultCode.FAIL)
+                .setMessage(message);
+    }
+}
---
+++ spring-boot-base/src/test/resources/generator/template/service.ftl
@@ -0,0 +1,12 @@
+package ${basePackage}.service;
+import ${basePackage}.model.${modelNameUpperCamel};
+import ${basePackage}.core.Service;
+
+
+/**
+ * @Author : ${author}
+ * @Date : ${date}
+ */
+public interface ${modelNameUpperCamel}Service extends Service<${modelNameUpperCamel}> {
+
+}
