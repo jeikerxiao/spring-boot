a0a4ed8fe8f05a849eb9f1901c38bda58c06a543
jeikerxiao <jeiker@126.com>
2017-08-02 09:33:06 +0800

    optimize dev config file
    optimize dev config file

--- spring-boot-base/src/main/java/com/jeiker/demo/SpringBootBaseApplication.java
+++ spring-boot-base/src/main/java/com/jeiker/demo/SpringBootBaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.jeiker.demo.dao")
+@MapperScan(basePackages = "com.jeiker.demo.dao")//扫描mapper
 public class SpringBootBaseApplication {
 
 	public static void main(String[] args) {
