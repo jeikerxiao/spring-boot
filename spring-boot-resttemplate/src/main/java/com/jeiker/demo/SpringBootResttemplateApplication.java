6527ec7ad457acd667b2742fc3378fc857914cf3
jeikerxiao <jeiker@126.com>
2017-09-18 15:58:39 +0800

    init restTemplate demo
    init restTemplate demo

---
+++ spring-boot-resttemplate/src/main/java/com/jeiker/demo/SpringBootResttemplateApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResttemplateApplication.class, args);
+	}
+}
