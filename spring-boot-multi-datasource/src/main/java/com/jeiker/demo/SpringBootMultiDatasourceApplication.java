a98c22ca5bf21e74f689170d46afe8bbb8052a2f
jeikerxiao <jeiker@126.com>
2017-09-21 07:58:19 +0800

    init spring-boot-multi-datasource project
    init spring-boot-multi-datasource project

---
+++ spring-boot-multi-datasource/src/main/java/com/jeiker/demo/SpringBootMultiDatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiDatasourceApplication.class, args);
+	}
+}
