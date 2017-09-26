46e0e3f052768abe80cfce2b7418061327003357
jeikerxiao <jeiker@126.com>
2017-09-26 14:50:45 +0800

    init spring-boot-log4j project
    init spring-boot-log4j project

---
+++ spring-boot-log4j/src/main/java/com/jeiker/demo/SpringBootLog4jApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLog4jApplication.class, args);
+	}
+}
