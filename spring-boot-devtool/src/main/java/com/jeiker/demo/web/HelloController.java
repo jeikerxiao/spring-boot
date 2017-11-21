9d95ac1f9bae4fbed249d30a19e886592ee02815
jeikerxiao <jeiker@126.com>
2017-11-21 09:51:13 +0800

    add HelloController
    add HelloController

--- spring-boot-devtool/src/main/java/com/jeiker/demo/web/HelloController.java
+++ spring-boot-devtool/src/main/java/com/jeiker/demo/web/HelloController.java
@@ -1,9 +1,21 @@
 package com.jeiker.demo.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * @author : xiao
  * @date : 17/11/21 上午9:19
  * @description :
  */
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Collections.singletonMap("message", "Hello World");
+    }
 }
