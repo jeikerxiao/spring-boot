8162925274055c8f56f71ff83c13286c29baff8a
jeikerxiao <jeiker@126.com>
2017-12-07 15:07:27 +0800

    init spring-boot-elastic
    init spring-boot-elastic

---
+++ spring-boot-elasticsearch5/src/main/java/com/jeiker/SpringBootElasticsearch5Application.java
@@ -0,0 +1,12 @@
+package com.jeiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearch5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearch5Application.class, args);
+	}
+}
