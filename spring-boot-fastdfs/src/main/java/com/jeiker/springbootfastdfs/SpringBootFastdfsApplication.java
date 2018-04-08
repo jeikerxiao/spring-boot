e10902d60128ae9031512d1692579426c1d36a2b
jeikerxiao <jeiker@126.com>
2018-04-08 16:41:15 +0800

    add project
    add project

---
+++ spring-boot-fastdfs/src/main/java/com/jeiker/springbootfastdfs/SpringBootFastdfsApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.springbootfastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFastdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFastdfsApplication.class, args);
+	}
+}
