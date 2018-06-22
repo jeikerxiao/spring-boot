2e816a5470b6e7f9701c9d2556d27f720a21488c
jeikerxiao <jeiker@126.com>
2018-06-22 14:23:30 +0800

    update redis controller demo

--- spring-boot-redis/src/main/java/com/jeiker/demo/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/com/jeiker/demo/config/RedisConfig.java
@@ -12,6 +12,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Description: spring-boot-study
@@ -31,13 +33,13 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
-        setSerializer(template);//设置序列化工具
+        setJsonSerializer(template);//设置序列化工具
         template.afterPropertiesSet();
         return template;
     }
 
     // json 序列化定义
-    private void setSerializer(StringRedisTemplate template) {
+    private void setJsonSerializer(StringRedisTemplate template) {
         @SuppressWarnings({"rawtypes", "unchecked"})
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
 
@@ -46,12 +48,12 @@ public class RedisConfig extends CachingConfigurerSupport {
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
 
-        // 定义key序列化方式
+        // 定义 key 的序列化方式
         // Long类型会出现异常信息;需要我们上面的自定义key生成策略，一般没必要
-//        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-//        template.setKeySerializer(redisSerializer);
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(redisSerializer);
 
-        // 设置value 的序列化方式为 json
+        // 定义 value 的序列化方式为 json
         template.setValueSerializer(jackson2JsonRedisSerializer);
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
 
--- spring-boot-redis/src/main/java/com/jeiker/demo/web/RedisController.java
+++ spring-boot-redis/src/main/java/com/jeiker/demo/web/RedisController.java
@@ -2,6 +2,7 @@ package com.jeiker.demo.web;
 
 import com.jeiker.demo.core.Result;
 import com.jeiker.demo.core.ResultGenerator;
+import com.jeiker.demo.model.City;
 import com.jeiker.demo.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +21,21 @@ public class RedisController {
 
     @RequestMapping("/save")
     public Result save() {
+        City city = new City();
+        city.setId(1);
+        city.setCountry("国家");
+        city.setName("城市名");
+        city.setState("省");
         //这里用于测试，key值可以自定义实现
-        redisService.set("123456", "test-redis");
-        return ResultGenerator.genSuccessResult();
+        redisService.set("city:4321", city);
+        return ResultGenerator.genSuccessResult(city);
     }
 
     @RequestMapping("/get")
     public Result get() {
         //这里用于测试，key值可以自定义实现
-        String value = (String) redisService.get("123456");
-        return ResultGenerator.genSuccessResult(value);
+        City city = (City) redisService.get("city:4312");
+        return ResultGenerator.genSuccessResult(city);
     }
 
 }
